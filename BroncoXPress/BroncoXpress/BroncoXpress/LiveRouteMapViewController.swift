70ad8262e7eca966d573d1072c032008b152fe36
Hetal <hssakaria@cpp.edu>
2015-10-29 21:27:37 -0700

    Each route displays their arrival time. Estimots Beacons now get detected and diplay their Route Name on the LiveMap

---
+++ BroncoXPress/BroncoXpress/BroncoXpress/LiveRouteMapViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LiveRouteMapViewController.swift
+//  BroncoXpress
+//
+//  Created by Hetal Sakaria on 10/29/15.
+//  Copyright © 2015 Hetal Sakaria. All rights reserved.
+//
+
+import Foundation
